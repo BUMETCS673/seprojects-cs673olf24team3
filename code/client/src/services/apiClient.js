07096bb2eb03097ecad21a7f3860829d6b1cc7e2
Chris Ceravolo <137582506+ccerav-bu@users.noreply.github.com>
2024-10-02 21:35:23 -0400

    third attempt

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,13 +1,7 @@
-import axios from 'axios';
-
-// console.log("API Base URL:", process.env.REACT_APP_BASE_URL);
-
-// const apiClient = axios.create({
-//   baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development
-// });
+import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://peak-performance-backend-394f316db343.herokuapp.com/', // Default to localhost for development
+  baseURL: process.env.REACT_APP_SERVER_URL
 });
 
 export default apiClient;
